89b58feddf34e271b8bfe84547a2be5e1b0e7b7e
0xChaddB <0xChaddB@proton.me>
2025-02-08 23:00:22 +0100

    scripts

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-# About
 
 1. Create a basic AA on Ethereum
 2. Create a basic AA on zkSync
---
+++ script/DeployMinimal.s.sol
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {MinimalAccount} from "lib/account-abstraction/contracts/MinimalAccount.sol";
